d763440d4352574690399a1360061925704dfe4b
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2023-12-12 05:35:12 +0000

    D6 - projects

--- 04-counter/src/App.jsx
+++ 04-counter/src/App.jsx
@@ -9,6 +9,13 @@ function App() {
       setCount(count + 1)
     }
   }
+
+  const counterVal = () => {
+    setCount(preCounter => preCounter + 1)
+    setCount(preCounter => preCounter + 1)
+    setCount(preCounter => preCounter + 1)
+    setCount(preCounter => preCounter + 1)
+  }
   const removeValue = () => {
     if(count > 0) {
       setCount(count - 1)
@@ -19,7 +26,8 @@ function App() {
       <h1>Counter Project</h1>
       <h2>Count Value : {count}</h2>
 
-      <button className={`${count == 20 && 'disabled'}`} onClick={addValue}>ADD VALUE {count}</button>
+      <button onClick={counterVal}>PREV COUNTER VALUE</button>
+      <button className={`${count >= 20 && 'disabled'}`} onClick={addValue}>ADD VALUE {count}</button>
       <button className={`${count == 0 && 'disabled'}`} onClick={removeValue}>REMOVE VALUE {count}</button>
     </div>
   )
